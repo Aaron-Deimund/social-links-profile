5eb6944bb7b6357447eb922c02ea670ff8319b83
Aaron Deimund <acdeimund@gmail.com>
2025-01-20 07:48:54 -0600

    Learning React. Adding portrait image.

--- src/components/reusable/UserBlock.tsx
+++ src/components/reusable/UserBlock.tsx
@@ -1,12 +1,14 @@
 import './UserBlock.css';
 
 function UserBlock(props:{
-	name:String
-	,location:String
-	,tagline:String
+	portraitUrl:string
+	,name:string
+	,location:string
+	,tagline:string
 }) {
   return (
     <div className='user-block'>
+		<img src={props.portraitUrl} alt={props.name + "-portrait"} />
 		<p className='text-preset-1'>{props.name}</p>
 		<p className='text-preset-2__bold'>{props.location}</p>
 		<p className='text-preset-2'>{props.tagline}</p>
--- src/components/semantic/ReactMain.tsx
+++ src/components/semantic/ReactMain.tsx
@@ -1,15 +1,16 @@
 import './ReactMain.css';
-
 import '../reusable/UserBlock';
 import UserBlock from '../reusable/UserBlock';
+import aaron from '../../assets/images/avatar-aaron.jpeg';
 
 function ReactMain() {
   return (
     <main>
 		<UserBlock 
-			name={"Aaron Deimund"} 
-			location={"Warrenton, Missouri"}
-			tagline={"Full-stack developer and avid TTRPG fan."}
+			portraitUrl= {aaron}
+			name="Aaron Deimund"
+			location="Warrenton, Missouri"
+			tagline="Full-stack developer and avid TTRPG fan."
 		/>
 		
 		
