691762b50a87b67b9850a626874554dfe7c8bc20
Aaron Deimund <acdeimund@gmail.com>
2025-01-27 15:29:23 -0600

    adding file extensions

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './app.css'
 
 // Semantic Components
-import ReactHeader from './components/semantic/ReactHeader'
-import ReactMain from './components/semantic/ReactMain';
-import ReactFooter from './components/semantic/ReactFooter';
+import ReactHeader from './components/semantic/ReactHeader.tsx'
+import ReactMain from './components/semantic/ReactMain.tsx';
+import ReactFooter from './components/semantic/ReactFooter.tsx';
 
 createRoot(document.querySelector("body")!).render(
   <StrictMode>
