bc8078ed1d827f8141deffb926fa1fa624f68296
Aaron Deimund <acdeimund@gmail.com>
2025-02-14 07:20:16 -0600

    adding hover color for links

--- src/components/reusable/LinkCustom.tsx
+++ src/components/reusable/LinkCustom.tsx
@@ -1,12 +1,11 @@
-import './linkCustom.css';
+import "./linkCustom.css";
 
-function linkCustom(props:{
-	url:string
-	text:string
-}) {
-  return (
-    <a className="custom-link text-preset-2__bold" href={props.url}>{props.text}</a>
-  )
+function linkCustom(props: { url: string; text: string }) {
+	return (
+		<a className="custom-link text-preset-2__bold" href={props.url}>
+			{props.text}
+		</a>
+	);
 }
 
 export default linkCustom;
